ba9ad13cbaaf221b506164c87b851bc5ef2d8565
Abhishek Srinivas <abhisrn@gmail.com>
2022-05-18 18:58:00 +0200

    Removed unnecessary method to connect to MongoDB

--- etl_job/tweets_database.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-def connect_to_mongodb():
-    # Establish a connection to the MongoDB server
-    client = pymongo.MongoClient(host="mongodb", port=27017, replicaset='dbrs')
-
-    # Select the database you want to use withing the MongoDB server
-    return client.twitter
